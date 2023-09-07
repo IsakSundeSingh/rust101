80f51ea76561ef25a24967e3f897f0f2150f4faa
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2023-09-07 14:15:03 +0200

    Add example on lifetimes

--- src/owners.rs
+++ src/owners.rs
@@ -44,3 +44,73 @@ fn exclusive_ownership(thing: &mut SomeType) {
     // to while this function runs.
     // This is checked compile time.
 }
+
+fn lifetimes() {
+    // `x` doesn't exist yet
+    {
+        let x = 42; // `x` starts existing
+        println!("x = {x}");
+        // `x` stops existing
+    }
+    // `x` no longer exists
+}
+
+fn reference_lifetimes() {
+    // `x` doesn't exist yet
+    {
+        let x = 42; // `x` starts existing
+        let x_ref = &x; // `x_ref` starts existing - it borrows `x`
+        println!("x_ref = {x_ref}");
+        // `x_ref` stops existing
+        // `x` stops existing
+    }
+    // `x` no longer exists
+}
+
+fn lifetimes_cannot_be_too_short() {
+    let x_ref = {
+        let x = 42;
+        &x
+    };
+    println!("x_ref = {x_ref}");
+    // error: `x` does not live long enough
+}
+
+fn exclusive_references_cannot_be_changed_by_others() {
+    let mut x = 42;
+    let x_ref = &x;
+    x = 13;
+    println!("x_ref = {x_ref}");
+    // error: cannot assign to `x` because it is borrowed
+}
+
+fn difference_between_owned_and_borrowed_types() {
+    let owned_string: String = String::from("hello");
+
+    let borrowed_string: &str = "there";
+    let borrowed_from_static: &'static str = borrowed_string;
+    // String literals live for as long as the program does
+    // as they are embedded in the binary. The two above have the
+    // same lifetime, but the last example has an explicit lifetime.
+}
+
+fn lifetimes_are_type_parameters<'this_is_a_lifetime_type_name_and_every_identifier_can_be_used>(
+    some_string: &'this_is_a_lifetime_type_name_and_every_identifier_can_be_used str,
+) {
+    // Any name can be used for lifetimes, but most of
+    // the times you don't have to write them explicitly
+    // Because of this, a lot of people just write 'a, 'b, and so on
+
+    // 'static is a special lifetime that lives the longest of them all.
+    // It is currently the only special lifetime
+}
+
+fn lifetimes_can_live_longer_than_others<'b, 'a: 'b>() {
+    // The above means that we have to generic lifetimes
+    // where 'a outlives 'b.
+    // This is rarely required.
+}
+
+struct StoredReference {
+    some_str: &str,
+}
